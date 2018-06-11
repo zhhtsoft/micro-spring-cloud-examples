c5bbebed31a42bbfd749e2d099395acbe17cc62b
ningquan <ningquan@aipark.com>
2018-06-11 21:01:37 +0800

    新增sleuth

--- microservice-sleuth/src/main/java/com/zhht/cloud/SleuthApplication.java
+++ microservice-sleuth/src/main/java/com/zhht/cloud/SleuthApplication.java
@@ -2,14 +2,12 @@ package com.zhht.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class EurekaApplication {
+public class SleuthApplication {
 
 	
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(SleuthApplication.class, args);
 	}
 }
